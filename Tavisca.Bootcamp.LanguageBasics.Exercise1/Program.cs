3f4fb0848345d4b9a82f2c54c9eaf5c8560cf5de
Rahul Tiwari <rtiwari@tavisca.in>
2019-06-22 22:17:46 +0530

    code

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,7 +22,48 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+                        string strings =equation;
+            string [] split = strings .Split(new Char [] {'*' , '=' });
+            float a=0,b=0,c=0,d=0,mul1=0;
+            int i=0;
+            string s="";
+            if(split[0].Contains("?") || split[1].Contains("?")){
+            if(split[0].Contains("?")){
+               i = split[0].IndexOf('?');
+               mul1=float.Parse(split[1]);
+               s=split[0];
+            }
+            else{
+               i = split[1].IndexOf('?');
+               mul1=float.Parse(split[0]);
+               s=split[1];
+            }
+            c= float.Parse(split[2]);
+            d=c/mul1;
+            if((d-(int)(d))==0){
+                strings = d.ToString();
+                if(i==0 && String.Equals(strings,s.Substring(1))){
+                    return -1;
+                }
+                return (int)Char.GetNumericValue(strings[i]);
+            }
+            else{
+                return -1;
+            }
+        }
+        else{
+            a=float.Parse(split[0]);
+            b=float.Parse(split[1]);
+            s=split[2];
+            i = split[2].IndexOf('?');
+            d=a*b;
+            strings = d.ToString();
+                if(i==0 && String.Equals(strings,s.Substring(1))){
+                    return -1;
+                }
+                return (int)Char.GetNumericValue(strings[i]);
+        }
+        
             throw new NotImplementedException();
         }
     }
